d0dcc465db7d60b338052c544d6caa7aadb7093c
zabalueva <age_eva@mail.ru>
2020-12-09 20:20:15 +0300

    Add days github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+
+
 
 function App() {
+  let [name, setName] = useState('');
+  let [avatar, setAvatar] = useState('');
+  let [dateCreate, setDate] = useState('');
+  let [days, setDays] = useState('');
+
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/zabalueva`)
+      .then(response => response.json())
+      .then(githubUserData => new Promise((resolve, reject) => {
+        setName(githubUserData.name);
+        setAvatar(githubUserData.avatar_url);
+        setDate(githubUserData.created_at); 
+      }))
+  }, [])
+  
+  function getDateCreate() {
+    setDays(() => { 
+      let currentDate = new Date();
+      let formatDate = new Date(Date.parse(dateCreate));
+      days = ((currentDate - formatDate) / 1000 / 60 / 60 / 24);
+      console.log(days.toFixed(0));
+  })}  
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p> Имяf: {name} </p>
+      <img src={avatar} alt={name}></img>
+      <button onClick={getDateCreate}>
+        Двигать вниз
+      </button>
+      {/* <p>{calcutDate.toFixed(0)} дней вы на гитхабе!</p> */}
+     
     </div>
   );
 }
