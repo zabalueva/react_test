5a56edfdb5429d8b839d4a078f36ba9b149034da
zabalueva <age_eva@mail.ru>
2020-12-12 21:49:31 +0300

    Add prompt (bug 'only one time')

--- src/App.js
+++ src/App.js
@@ -5,11 +5,17 @@ function App() {
   let [avatar, setAvatar] = useState('');
   let [days, setDays] = useState('');
 
+  function nameInput() {
+    const nameInputed = prompt('Введите ваше имя на github', "username");
+    return nameInputed;
+  }
 
-  useEffect(() => {
-    fetch(`https://api.github.com/users/zabalueva`)
+  useEffect(() => {   
+    const user = nameInput();
+    fetch(`https://api.github.com/users/${user}`)
       .then(response => response.json())
       .then(githubUserData => new Promise((resolve, reject) => {
+
         setName(githubUserData.login);
         setAvatar(githubUserData.avatar_url); 
 
@@ -19,26 +25,15 @@ function App() {
         setDays(calc.toFixed(0));
       }))
   }, [])
-  
-  /* function getDateCreate() {
-    setDays(() => { 
-      let currentDate = new Date();
-      let formatDate = new Date(Date.parse(dateCreate));
-      days = ((currentDate - formatDate) / 1000 / 60 / 60 / 24);
-      console.log(`${days.toFixed(0)} дня вы на GitHub!`);
-  })}  */ 
-
 
   return (   
-    <div className="App">     
+    <div className="App">  
+      <button onClick={nameInput}>
+                Получить подарочную информацию!
+      </button>  
       <p> Имя: {name} </p>
-      <p> Вы на github уже {days}! </p>
-      <img src={avatar} alt={name} className='avatar'></img>
-      {/* <button onClick={getDateCreate}>
-        Получить подарочную информацию!
-      </button> */}
-      {/* <p>{calcutDate.toFixed(0)} дней вы на гитхабе!</p> */}
-    
+      <p> Вы на github уже {days} дня! </p>
+      <img src={avatar} alt={name} className='avatar'></img>    
     </div>
   );
 }
